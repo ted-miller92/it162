25447c8c5bff619f8a972bd7b73299021d8619bc
Ted Miller <ted.miller92@gmail.com>
2021-04-23 15:26:18 -0700

    April 22 Commit - adjusted include php header and footer

--- contactme.php
+++ contactme.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial scale=1">
-		<title>Ted's IT162 Portal</title>
-		<link rel="stylesheet" href="css/reset.css">
-		<link rel="stylesheet" href="css/styles.css">
-		<!--Fonts-->
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Libre+Franklin&display=swap" rel="stylesheet">
-		<!-- Icon library to show a hamburger menu (bars) on small screens -->
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	</head>
-	<body>
 		<!--this will be header.php-->
 		<?php include "includes/header.php";?>
 		<!--End header.php-->
+		<div id="formWrapper">
 		<h2>Contact Ted</h2>
-		<article>
-        </article>
+		<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "ted@scriptedmiller.com";  //place your/your client's email address here
+        $toName = "Ted"; //place your client's name here
+        $website = "www.scriptedmiller.com";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
+        </div>
 		<!--Start Footer-->
 		<?php include "includes/footer.php";?>
 		<!--End Footer-->
--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,27 @@
-<h1>Ted Miller's IT162 Portal</h1>
-	<div class="topnav" id="myTopnav">
-		<a href="index.php" class="active">Home</a>
-		<a href="big/index.php">Big</a>
-		<a href="aia.php">AIA</a>
-		<a href="flowchart.php">Flowchart</a>
-		<a href="fp/index.php">Final Project</a>
-		<a href="contactme.php">Contact Ted</a>
-		<a href="javascript:void(0);" class="icon" onclick="myFunction()">
-		<i class="fa fa-bars"></i>
-		</a>
-	</div>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<title>Ted's IT162 Portal</title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/forms.css">
+		<!--Fonts-->
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Libre+Franklin&display=swap" rel="stylesheet">
+		<!-- Icon library to show a hamburger menu (bars) on small screens -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<h1>Ted Miller's IT162 Portal</h1>
+			<div class="topnav" id="myTopnav">
+				<a href="index.php" class="active">Home</a>
+				<a href="big/index.php">Big</a>
+				<a href="aia.php">AIA</a>
+				<a href="flowchart.php">Flowchart</a>
+				<a href="fp/index.php">Final Project</a>
+				<a href="contactme.php">Contact Ted</a>
+				<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+				<i class="fa fa-bars"></i>
+				</a>
+			</div>
