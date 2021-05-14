bfdaf56aae0721f3df4ef7b8b815f7fb0eaa6446
Ted Miller <ted.miller92@gmail.com>
2021-05-13 23:18:33 -0700

    Commit with AIA updates

--- aia.php
+++ aia.php
@@ -1,9 +1,102 @@
-<!DOCTYPE html>
 		<!--this will be header.php-->
 		<?php include "includes/header.php";?>
 		<!--End header.php-->
 		<h2>AIA</h2>
 		<article>
+			<table>
+				<caption>Client Information</caption>
+				<tr>
+					<th>Client Name</th>
+					<td>Paul Danos, Mckenzie Hart, Dustin Riggs</td>
+				</tr>
+				<tr>
+					<th>Client Business</th>
+					<td>Peloton Cafe (Bike shop/Cafe)</td>
+				</tr>
+				<tr>
+					<th>Redesign/Brand New site?</th>
+					<td>Redesign</td>
+				</tr>
+				<tr>
+					<th>Type of Website</th>
+					<td>E-commerce, landing page for business, Potentially used for online food ordering in the future</td>
+				</tr>
+				<tr>
+					<th>Client's Goals</th>
+					<td>Redesign and update current website, include more about services</td>
+				</tr>
+			</table>
+			<br>
+			
+			<table>
+				<caption>AIA</caption>
+				<tr>
+					<th>Audience</th>
+					<th>Interest</th>
+					<th>Approach</th>
+				</tr>
+				<tr>
+					<td>
+						<h3>Cafe customers</h3>
+						<p>Cafe customers will be all ages, and may access the website from any device, though it is likely they will be using a mobile device, and with that consideration they may have limited bandwidth (mobile service). Because of the variety of potential customers, they will need things to be pretty clear and easy to use, especially if online ordering is implemented.</p>
+					</td>
+					<td>
+						<ul>
+							<li>View a Menu</li>
+							<li>View photos of menu items</li>
+							<li>Order food</li>
+							<li>View location and hours</li>
+						</ul>
+					</td>
+					<td>
+						<ul>
+							<li>Make sure the online ordering service is very easily accessible</li>
+							<li>Make sure location and hours are easy to see</li>
+							<li>Currently ordering is done through Square, so a link to the square ordering site may suffice</li>
+							<li>A menu or showcase of menu items should be present.</li>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<h3>Bike Service Customers</h3>
+						<p>Bike shop customers will also be all ages. Again they will be using a variety of devices.</p>
+					</td>
+					<td>
+						<ul>
+							<li>View prices and services offered</li>
+							<li>View examples of past work</li>
+							<li>View examples of trustworthiness/experience etc. </li>
+						</ul>
+					</td>
+					<td>
+						<ul>
+							<li>Show a list/menu of services offered with an accurate and updateable list of prices. </li>
+							<li>It will also be important to link to the “contact” page and make it clear how customers should initiate a service </li>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<h3>Bike Build Customers</h3>
+						<p>Customers that are interested in having a custom bike build. This customer will again be of a variety of ages/backgrounds, and will be using a variety of devices</p>
+					</td>
+					<td>
+						<ul>
+							<li>View examples of previous bike builds</li>
+							<li>View brands and manufacturers available</li>
+							<li>Price estimates</li>
+						</ul>
+					</td>
+					<td>
+						<ul>
+							<li>This might be a good place for a gallery of photos of previous builds</li>
+							<li>Include a list/gallery of brands and manufacturers that the shop deals with </li>
+							<li>It will be important to highlight how to contact the shop. </li>
+						</ul>
+					</td>
+				</tr>
+			</table>
         </article>
 		<!--Start Footer-->
 		<?php include "includes/footer.php";?>
