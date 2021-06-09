4244b371bbbcbea094db8ed1b2795a67c3792949
Ted Miller <ted.miller92@gmail.com>
2021-06-08 23:05:07 -0700

    added/perfected gallery

--- fp/js/nav.js
+++ fp/js/nav.js
@@ -1,12 +1,12 @@
 var menuButton = document.getElementById("menuButton");
 var mobileMenu = document.getElementById("mobileMenu");
+var mainContent = document.getElementById("mainContent");
 
 function openMenu(){
+    //show menu
     if (mobileMenu.classList.contains("visible")) {
         mobileMenu.classList.remove("visible");
     }else{
         mobileMenu.classList.add("visible");
     }
 }
-
-menuButton.addEventListener("click", openMenu);
---
+++ fp/js/stripes.js
@@ -0,0 +1,16 @@
+//Making stripes
+var colors = ['#0077c8','#c8102e', '#111', '#fedd00', '#43b02a'];   //array holds the five hex values of colors
+
+for (var i = 0; i < colors.length; i++){                            //loop that creates a div of a color and appends it to the "stripe container"
+    var stripe = document.createElement('div');
+    stripe.style.backgroundColor = colors[i];
+    // stripe.style.height = '3vw';
+    if (window.innerWidth < 1024){
+        stripe.style.height = '3vw';
+    }else{
+        stripe.style.height = '1vw';
+    }
+    var stripeContainer = document.getElementById('stripes');
+    stripeContainer.appendChild(stripe);
+}
+menuButton.addEventListener("click", openMenu);
